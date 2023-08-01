346f63eab235c3784356722617d1fca0a416ef85
Greene161 <76180847+Greene161@users.noreply.github.com>
2023-08-01 14:46:13 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,54 @@
+button = document.getElementsByClassName("btn");
+
+addEventListener('click', button);
+
+function Monday() {
+   var target = document.getElementById("li1");
+   if(target.style.display=="none") {
+      target.style.display="grid";
+   }else {
+      target.style.display="none";
+   }
+}
+
+function Tuesday() {
+   var target = document.getElementById("li2");
+   if(target.style.display=="none") {
+      target.style.display="grid";
+   }else {
+      target.style.display="none";
+   }
+}
+
+
+function Wednesday() {
+   var target = document.getElementById("li3");
+   if(target.style.display=="none") {
+      target.style.display="grid";
+   }else {
+      target.style.display="none";
+   }
+}
+
+
+
+function Thursday() {
+   var target = document.getElementById("li4");
+   if(target.style.display=="none") {
+      target.style.display="grid";
+   }else {
+      target.style.display="none";
+   }
+}
+
+
+
+function Friday() {
+   var target = document.getElementById("li5");
+   if(target.style.display=="none") {
+      target.style.display="grid";
+   }else {
+      target.style.display="none";
+   }
+   
+}
