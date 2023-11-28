08126cd2369d5f013fdbca6637e7739563687ddb
Greene161 <76180847+Greene161@users.noreply.github.com>
2023-11-28 12:48:35 -0800

    Update java.js

--- java.js
+++ java.js
@@ -8,13 +8,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const timetableContainer = document.getElementById("timetable");
 
-    // Dynamically generate timetable HTML
+    // Dynamically generate timetable HTML using Bootstrap classes
     timetableData.forEach(entry => {
-        const entryHtml = `<div class="timetable-entry">
-                                <h2>${entry.course}</h2>
-                                <p>Date: ${entry.date}</p>
-                                <p>Time: ${entry.time}</p>
+        const entryHtml = `<div class="card mb-3">
+                                <div class="card-body">
+                                    <h2 class="card-title">${entry.course}</h2>
+                                    <p class="card-text">Date: ${entry.date}</p>
+                                    <p class="card-text">Time: ${entry.time}</p>
+                                </div>
                             </div>`;
         timetableContainer.innerHTML += entryHtml;
+    });
+
+    // Add interactivity using Bootstrap's collapse component
+    const timetableEntries = document.querySelectorAll('.card');
+
+    timetableEntries.forEach(entry => {
+        entry.addEventListener('click', function() {
+            // Toggle the collapse class when a timetable entry is clicked
+            this.classList.toggle('collapsed');
+        });
     });
 });
