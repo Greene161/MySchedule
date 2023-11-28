4b871a130a18386f85776cac2ba83d6feec306ad
Greene161 <76180847+Greene161@users.noreply.github.com>
2023-11-28 14:18:10 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,32 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Replace this data with your actual exam timetable
+    // Sample timetable data
     const timetableData = [
-        { course: "Math", date: "2023-12-01", time: "10:00 AM" },
-        { course: "History", date: "2023-12-02", time: "02:00 PM" },
-        // Add more entries as needed
+      { course: "Electricity & Magnetism", time: "09:00 AM - 12:00 PM", room: "HL&LR2", day: "Thursday, 14/12/2023" },
+      { course: "Higher Mathematics II", time: "09:00 AM - 12:00 PM", room: "EHL", day: "Friday, 15/12/2023" },
+      { course: "Electronics for Computing I", time: "09:00 AM - 12:00 PM", room: "HL", day: "Tuesday, 19/12/2023" },
+      { course: "Operating Systems", time: "02:00 PM - 05:00 PM", room: "HL", day: "Wednesday, 20/12/2023" },
+      { course: "Basic Electronics I", time: "02:00 PM - 05:00 PM", room: "LR1", day: "Friday, 22/12/2023" }
     ];
-
+  
+    // Get the timetable container
     const timetableContainer = document.getElementById("timetable");
-
-    // Dynamically generate timetable HTML using Bootstrap classes
-    timetableData.forEach(entry => {
-        const entryHtml = `<div class="card mb-3">
-                                <div class="card-body">
-                                    <h2 class="card-title">${entry.course}</h2>
-                                    <p class="card-text">Date: ${entry.date}</p>
-                                    <p class="card-text">Time: ${entry.time}</p>
-                                </div>
-                            </div>`;
-        timetableContainer.innerHTML += entryHtml;
+  
+    // Populate the timetable
+    timetableData.forEach(course => {
+      const card = document.createElement("div");
+      card.className = "col-md-4 mb-4";
+  
+      card.innerHTML = `
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${course.course}</h5>
+            <p class="card-text">${course.time}</p>
+            <p class="card-text">${course.room}</p>
+            <p class="card-text">${course.day}</p>
+          </div>
+        </div>
+      `;
+  
+      timetableContainer.appendChild(card);
     });
-
-    // Add interactivity using Bootstrap's collapse component
-    const timetableEntries = document.querySelectorAll('.card');
-
-    timetableEntries.forEach(entry => {
-        entry.addEventListener('click', function() {
-            // Toggle the collapse class when a timetable entry is clicked
-            this.classList.toggle('collapsed');
-        });
+  
+    // Event delegation to handle the "Fade Out" button click
+    timetableContainer.addEventListener("click", function(event) {
+      const fadeOutButton = event.target.closest(".fade-out-btn");
+      if (fadeOutButton) {
+        fadeOutCard(fadeOutButton);
+      }
     });
-});
+  
+    // Function to handle the "Fade Out" button click
+    function fadeOutCard(button) {
+      const card = button.closest('.card');
+      card.style.opacity = 0.5;
+      button.disabled = true;
+    }
+  });
+  
