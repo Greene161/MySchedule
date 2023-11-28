26bd5d006fcdd67b28523461e816da56bda53f49
Greene161 <76180847+Greene161@users.noreply.github.com>
2023-11-28 12:41:08 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,54 +1,20 @@
-button = document.getElementsByClassName("btn");
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace this data with your actual exam timetable
+    const timetableData = [
+        { course: "Math", date: "2023-12-01", time: "10:00 AM" },
+        { course: "History", date: "2023-12-02", time: "02:00 PM" },
+        // Add more entries as needed
+    ];
 
-addEventListener('click', button);
+    const timetableContainer = document.getElementById("timetable");
 
-function Monday() {
-   var target = document.getElementById("li1");
-   if(target.style.display=="none") {
-      target.style.display="grid";
-   }else {
-      target.style.display="none";
-   }
-}
-
-function Tuesday() {
-   var target = document.getElementById("li2");
-   if(target.style.display=="none") {
-      target.style.display="grid";
-   }else {
-      target.style.display="none";
-   }
-}
-
-
-function Wednesday() {
-   var target = document.getElementById("li3");
-   if(target.style.display=="none") {
-      target.style.display="grid";
-   }else {
-      target.style.display="none";
-   }
-}
-
-
-
-function Thursday() {
-   var target = document.getElementById("li4");
-   if(target.style.display=="none") {
-      target.style.display="grid";
-   }else {
-      target.style.display="none";
-   }
-}
-
-
-
-function Friday() {
-   var target = document.getElementById("li5");
-   if(target.style.display=="none") {
-      target.style.display="grid";
-   }else {
-      target.style.display="none";
-   }
-   
-}
+    // Dynamically generate timetable HTML
+    timetableData.forEach(entry => {
+        const entryHtml = `<div class="timetable-entry">
+                                <h2>${entry.course}</h2>
+                                <p>Date: ${entry.date}</p>
+                                <p>Time: ${entry.time}</p>
+                            </div>`;
+        timetableContainer.innerHTML += entryHtml;
+    });
+});
